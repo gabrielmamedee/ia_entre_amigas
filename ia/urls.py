3c978e4338da8dc31ab975d0318a53d779dbed46
gabrielmamedee <mamede.dev@gmail.com>
2023-05-16 12:35:58 -0300

    alteração na url

--- ia/urls.py
+++ ia/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('nuvem/', include('word_cloud.urls'))
+    path('', include('word_cloud.urls'))
 ]
--- word_cloud/views.py
+++ word_cloud/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 import json
 
 def view_the_cloud(request):
+     
      if request.method == "GET":
           return render(request, 'index.html')
+     
      elif request.method == "POST":
           palavras = request.POST.get('valor')
           print(palavras)
