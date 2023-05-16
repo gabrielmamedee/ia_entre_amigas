fd9de56b9189e0436248609bf45fa6b7812c3a73
gabrielmamedee <mamede.dev@gmail.com>
2023-05-16 12:18:35 -0300

    finalização do front

--- word_cloud/views.py
+++ word_cloud/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 import json
 
 def view_the_cloud(request):
-     my_data = {'p1': 'cidadeviva', 'p2': 'bar'}
-     return render(request, 'index.html', {'my_data': my_data})
+     if request.method == "GET":
+          return render(request, 'index.html')
+     elif request.method == "POST":
+          palavras = request.POST.get('valor')
+          print(palavras)
+          return render(request, 'pesquisa.html', {'palavras':palavras})
